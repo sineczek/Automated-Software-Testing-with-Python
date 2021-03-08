b9fb3eda4261f39b4008195c0e788354e776c4c5
Michal Zaitz <michalzaitz@gmail.com>
2021-03-08 19:25:10 +0100

    dziś szybka piła - booleans

---
+++ Python Refresher/07_booleans.py
@@ -0,0 +1,16 @@
+print(5 == 5)  # boolean
+print(5 > 5)
+print(10 != 10)
+
+# comparisons: ==, !=, <, >, <=, >=
+
+friends = ['Rolf', 'Bob']
+abrod = ['Rolf','Bob']
+
+print(friends == abrod) #true
+
+print(friends is abrod) #false, bo różne obszary w pamięci, 2 listy
+
+
+
+
