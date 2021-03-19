5b8f692f8bfbd95c1bfeb499a8c7d6b9e9966d1a
Michal Zaitz <michalzaitz@gmail.com>
2021-03-19 18:43:57 +0100

    Dzień #81 - urodzinowy
    - imports

---
+++ Python Refresher/32_relative_imports.py
@@ -0,0 +1,13 @@
+"""absolutny import
+
+import mymodule
+
+print("code.py", __name__)"""
+
+# relatywny import
+# z folderów się robi
+from .31_import import mymodule
+
+divide(3, 4)
+
+#lepiej tego nie używać, ale wiedzieć dobrze
