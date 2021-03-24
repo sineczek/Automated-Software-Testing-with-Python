fa82dedf38a0099b8ea133e5ef598d4995189192
Michal Zaitz <michalzaitz@gmail.com>
2021-03-24 18:39:01 +0100

    Dzień #86
    - testy integracyjne

--- First_automated_software_test/blog/blog.py
+++ First_automated_software_test/blog/blog.py
@@ -1,3 +1,6 @@
+from post import Post
+
+
 class Blog:
     def __init__(self, title, author):
         self.author = author
@@ -11,10 +14,11 @@ class Blog:
                                              's' if len(self.posts) != 1 else '')
 
     def create_post(self, title, content):
-        pass
+        self.posts.append(Post(title, content))
 
     def json(self):
         return {
             'title': self.title,
-            'content': self.content,
+            'author': self.author,
+            'posts': [post.json() for post in self.posts]
         }
